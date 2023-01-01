0d734523cc951b2907a90d17f61431682ccb89aa
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-01 15:46:04 +0200

    header+cta

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import CTA from "./CTA";
 import "./header.css";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Moti Charlap</h1>
+        <h5>Fullstack Developer</h5>
+        <CTA />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
