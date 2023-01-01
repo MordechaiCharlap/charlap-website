1f9071cea037bbddb58ae422681727ab5a4e976a
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-01 15:45:58 +0200

    cta init

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CTA = () => {
+  return (
+    <div>
+      <a href="https://google.com" className="btn btn-primary">
+        Download CV
+      </a>
+      <a href="https://google.com" className="btn btn-primary">
+        Let's Talk
+      </a>
+    </div>
+  );
+};
+
+export default CTA;
