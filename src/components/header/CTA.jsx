735884f98cdb978a6a37f4743e44d46a684ff781
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-01 19:37:17 +0200

    margin

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 const CTA = () => {
+  const margin = {
+    margin: "0.5rem",
+  };
   return (
     <div>
-      <a href="https://google.com" className="btn btn-primary">
+      <a href="https://google.com" className="btn" style={margin}>
         Download CV
       </a>
-      <a href="https://google.com" className="btn btn-primary">
+      <a href="https://google.com" className="btn btn-primary" style={margin}>
         Let's Talk
       </a>
     </div>
