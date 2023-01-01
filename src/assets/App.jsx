c214b0b247d3afae4263b13bff59ee6e93a10480
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-01 12:32:41 +0200

    all basic comps impoted

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import About from "../components/about/About";
+import Contact from "../components/contact/Contact";
+import Experience from "../components/experience/Experience";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <About />
+      <Contact />
+      <Experience />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
