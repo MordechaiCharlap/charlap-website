d23fdebef38733f252da7c3363d5224601035f14
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-01 19:37:27 +0200

    header socials + picture added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import CTA from "./CTA";
 import "./header.css";
+import ME from "../../assets/robot-guy.png";
+import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
     <header>
@@ -9,6 +11,16 @@ const Header = () => {
         <h1>Moti Charlap</h1>
         <h5>Fullstack Developer</h5>
         <CTA />
+
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="Just me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
