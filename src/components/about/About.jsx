3b12e96d3cdc2a1d214ab7fe13a73e01d3322886
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-02 12:02:43 +0200

    about html

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 import "./about.css";
+import Me from "../../assets/me.jpg";
+import { FaAward } from "react-icons/fa";
 const About = () => {
-  return <section id="about"></section>;
+  return (
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Me} alt={"About Image"} />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3 Years</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
