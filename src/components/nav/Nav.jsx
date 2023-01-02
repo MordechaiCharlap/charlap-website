62052190957aa54273d6cb372e3ca884855b8bb8
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-02 11:10:46 +0200

    icons+a

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { BiMessageSquare } from "react-icons/bi";
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <a href="#">
+        <AiOutlineHome />
+      </a>
+      <a href="#about">
+        <AiOutlineUser />
+      </a>
+      <a href="#exprience">
+        <BiBook />
+      </a>
+      <a href="#portfolio">
+        <RiServiceLine />
+      </a>
+      <a href="#contact">
+        <BiMessageSquare />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
