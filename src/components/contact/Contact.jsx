e212b78d74bea0fa0f1176bc3a7e6e527238a2ea
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-03 12:18:33 +0200

    contact init

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./contact.css";
 const Contact = () => {
-  return <section id="contact"></section>;
+  return (
+    <section id="contact">
+      <div className="container contact__container">
+        <div className="about__title">
+          <h1>Contact</h1>
+          <h4>
+            Feel free to Contact me by submitting the form below and I will get
+            back to you as soon as possible
+          </h4>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
