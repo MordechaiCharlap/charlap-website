f70b60e023bbe8eadd8ee93826f89c2f09cc6ffa
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-03 12:41:21 +0200

    contact styling+form building

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,12 +4,22 @@ const Contact = () => {
   return (
     <section id="contact">
       <div className="container contact__container">
-        <div className="about__title">
+        <div className="contact__title">
           <h1>Contact</h1>
           <h4>
             Feel free to Contact me by submitting the form below and I will get
             back to you as soon as possible
           </h4>
+        </div>
+        <div className="contact__form__container">
+          <form>
+            <h5>Name</h5>
+            <input placeholder="Enter Your Name"></input>
+            <h5>Email</h5>
+            <input placeholder="Enter Your Email"></input>
+            <h5>Message</h5>
+            <input placeholder="Enter Your Message"></input>
+          </form>
         </div>
       </div>
     </section>
