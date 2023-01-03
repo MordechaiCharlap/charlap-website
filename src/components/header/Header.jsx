33f4c95b82d1dfb5dcb86097ed74700a067a1b72
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-03 10:10:38 +0200

    robot guyy

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CTA from "./CTA";
 import "./header.css";
-import ME from "../../assets/me.jpg";
+import ME from "../../assets/robot-guy.png";
 import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
@@ -9,7 +9,11 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Moti Charlap</h1>
-        <h5>Fullstack Developer</h5>
+        <h5>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          exercitationem delectus corporis rem eveniet, animi repellat nostrum?
+          Maiores voluptates cupiditate ipsam et excepturi.
+        </h5>
         <CTA />
         <div className="me">
           <img src={ME} alt="Just me" />
