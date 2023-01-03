dbdf1c173623420d675d65c5eddb775f47f9d51f
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-03 14:19:09 +0200

    submissions are going to firestore

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.css";
+import { firestore } from "../../firebase.config.js";
+import { addDoc, collection } from "firebase/firestore";
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const submitForm = async () => {
+    if (name != "" && validateEmail(email) && message.length > 5) {
+      const db = firestore;
+      const submission = {
+        senderName: name,
+        senderEmail: email,
+        message: message,
+      };
+      await addDoc(collection(db, "submissions"), {
+        ...submission,
+      });
+    } else {
+      console.log("not a valid submission");
+    }
+  };
+
+  const validateEmail = (mail) => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+      return true;
+    }
+    console.log("not a valid email");
+    return false;
+  };
   return (
     <section id="contact">
       <div className="container contact__container">
         <div className="contact__title">
-          <h1>Contact</h1>
+          <h1>CONTACT</h1>
           <h4>
             Feel free to Contact me by submitting the form below and I will get
             back to you as soon as possible
@@ -14,17 +42,26 @@ const Contact = () => {
         <div className="contact__form__container">
           <form>
             <h5>Name</h5>
-            <input placeholder="Enter Your Name"></input>
+            <input
+              onChange={(event) => setName(event.target.value)}
+              placeholder="Enter Your Name"
+            ></input>
             <h5>Email</h5>
-            <input placeholder="Enter Your Email"></input>
+            <input
+              onChange={(event) => setEmail(event.target.value)}
+              placeholder="Enter Your Email"
+            ></input>
             <h5>Message</h5>
             <textarea
+              onChange={(event) => setMessage(event.target.value)}
               rows={10}
               className="messageInput"
               placeholder="Enter Your Message"
             ></textarea>
           </form>
-          <a className="btn">Submit</a>
+          <a className="btn" onClick={submitForm}>
+            Submit
+          </a>
         </div>
       </div>
     </section>
