0b5edf8959ae41e3308c29007088da8eee1e1def
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-03 11:29:28 +0200

    Skills craeted

---
+++ src/components/about/Skills.jsx
@@ -0,0 +1,35 @@
+import React, { createElement } from "react";
+
+const Skills = () => {
+  const getSkills = () => {
+    const skills = [
+      "JavaScript",
+      "C#",
+      "HTML/CSS",
+      "React",
+      "Firebase",
+      "React Native",
+      "Github",
+      "SQL",
+      "Photoshop",
+    ];
+    const skillCards = [];
+    for (var skillValue of skills) {
+      const skillCard = (
+        <div key={skillValue} className="skill__card">
+          <h4>{skillValue}</h4>
+        </div>
+      );
+      skillCards.push(skillCard);
+    }
+    return <div className="skill__cards">{skillCards}</div>;
+  };
+  return (
+    <div className="skills__container">
+      <h2>My Skills</h2>
+      {getSkills()}
+    </div>
+  );
+};
+
+export default Skills;
