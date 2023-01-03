909bc019e089daf0a7498e205df8b901949cbcf1
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-03 12:12:26 +0200

    projects kickstart

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./portfolio.css";
 const Portfolio = () => {
-  return <section id="portfolio"></section>;
+  return (
+    <section id="portfolio">
+      <h1>PROJECTS</h1>
+      <div className="portfolio__container container"></div>
+    </section>
+  );
 };
 
 export default Portfolio;
