7631f7022899d5009d6909caa04844cc858228c3
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-16 20:29:30 +0200

    Finished 4 cards

--- src/components/portfolio/workouteer/Workouteer.jsx
+++ src/components/portfolio/workouteer/Workouteer.jsx
@@ -9,37 +9,47 @@ import ProjectCard from "../ProjectCard";
 const Workouteer = () => {
   return (
     <div>
-      <div className="project__header">
-        <div className="project__image">
-          <img src={workouteerIcon} alt="Workouteer icon" />
+      <div className="project__header__links">
+        <div className="project__header__container">
+          <div className="project__image">
+            <img src={workouteerIcon} alt="Workouteer icon" />
+          </div>
+          <div className="project__title">
+            <h3>Workouteer: Social app for outdoor fitness enthusiasts</h3>
+          </div>
         </div>
-        <div className="project__title">
-          <h3>Workouteer: Social app for outdoor fitness enthusiasts</h3>
-        </div>
-        <div>
-          <a href="https://google.com" className="btn btn-primary">
-            Download APK file!
-          </a>
+
+        <div className="project__links">
+          <div>
+            <a href="https://google.com" className="btn btn-primary">
+              Download APK file!
+            </a>
+          </div>
+          <div>
+            <a href="https://workouteer.co.il" className="btn">
+              {"Or check the Website version :)"}
+            </a>
+          </div>
         </div>
       </div>
       <div className="screenshot__cards">
         <ProjectCard
-          title="Schedule a workout!"
+          title="Schedule a workout"
           subTitle="Choose type, time and place"
           screenshotImage={createWorkout}
         />
         <ProjectCard
-          title="Find your next workout partner!"
+          title="Detailed workouts tracking"
           subTitle="Chat with people who share the same hobbies"
           screenshotImage={workoutDetails}
         />
         <ProjectCard
-          title="Find workouts in your area!"
+          title="Find workouts in your area"
           subTitle="Find workouts by city and distance from your current location"
           screenshotImage={findWorkouts}
         />
         <ProjectCard
-          title="Find your next workout partner!"
+          title="Find your next workout partner"
           subTitle="Chat with people who share the same hobbies"
           screenshotImage={chat}
         />
