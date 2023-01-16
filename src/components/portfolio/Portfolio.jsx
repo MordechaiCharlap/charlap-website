e6eb777a490e05a4c210d8e6bb6d1cb1c5a23400
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-16 16:09:08 +0200

    Workouteer added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./portfolio.css";
+import Workouteer from "./workouteer/Workouteer";
 const Portfolio = () => {
   return (
     <section id="portfolio">
       <h1>PROJECTS</h1>
-      <div className="portfolio__container container"></div>
+      <div className="portfolio__container container">
+        <Workouteer />
+      </div>
     </section>
   );
 };
