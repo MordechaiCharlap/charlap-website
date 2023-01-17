85243c15d797f61553e390e855d0cd50e054b18b
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-17 10:07:03 +0200

    Carousel implemented

--- src/components/portfolio/workouteer/Workouteer.jsx
+++ src/components/portfolio/workouteer/Workouteer.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import "./workouteer.css";
 import workouteerIcon from "./assets/icon/app-icon.png";
-import createWorkout from "./assets/screenshots/create-workout.jpg";
 import findWorkouts from "./assets/screenshots/find-workouts.jpg";
 import chats from "./assets/screenshots/chats.jpg";
 import workoutDetails from "./assets/screenshots/workout-details.jpg";
+import pastWorkouts from "./assets/screenshots/past-workouts.jpg";
 import apkUrl from "./assets/apk/Workouteer.apk";
 import ProjectCard from "../ProjectCard";
+import Carousel from "react-bootstrap/Carousel";
+import Button from "react-bootstrap/Button";
+import useCheckMobileScreen from "../../../hooks/useCheckMobileScreen";
 const Workouteer = () => {
+  const isMobile = useCheckMobileScreen();
+  console.log(isMobile);
   return (
     <div>
+      <Button as="a" variant="success">
+        Button as link
+      </Button>
       <div className="project__header__links">
         <div className="project__header__container">
           <div className="project__image">
@@ -22,7 +30,7 @@ const Workouteer = () => {
 
         <div className="project__links">
           <div>
-            <a download href={apkUrl} className="btn btn-primary">
+            <a download href={apkUrl} className="btn btn-primary project__link">
               Download APK file!
             </a>
           </div>
@@ -33,28 +41,80 @@ const Workouteer = () => {
           </div>
         </div>
       </div>
-      <div className="screenshot__cards">
-        <ProjectCard
-          title="Schedule a workout"
-          subTitle="Choose type, time and place"
-          screenshotImage={createWorkout}
-        />
-        <ProjectCard
-          title="Detailed workouts tracking"
-          subTitle="Chat with people who share the same hobbies"
-          screenshotImage={workoutDetails}
-        />
-        <ProjectCard
-          title="Find workouts in your area"
-          subTitle="Find workouts by city and distance from your current location"
-          screenshotImage={findWorkouts}
-        />
-        <ProjectCard
-          title="Find your next workout partner"
-          subTitle="Chat with people who share the same hobbies"
-          screenshotImage={chats}
-        />
-      </div>
+      {!isMobile ? (
+        <div className="screenshot__cards">
+          <ProjectCard
+            title="Detailed workouts documenting"
+            subTitle="The exact location shared between your friends"
+            screenshotImage={workoutDetails}
+          />
+          <ProjectCard
+            title="Find workouts in your area"
+            subTitle="Find workouts by city and distance from your current location"
+            screenshotImage={findWorkouts}
+          />
+          <ProjectCard
+            title="Track your workouts history"
+            subTitle="Workouteer helps you keep your exercise routine going"
+            screenshotImage={pastWorkouts}
+          />
+          <ProjectCard
+            title="Find your next workout partner"
+            subTitle="Chat with people who share the same hobbies"
+            screenshotImage={chats}
+          />
+        </div>
+      ) : (
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={workoutDetails}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={findWorkouts}
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={pastWorkouts}
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={chats} alt="Third slide" />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      )}
     </div>
   );
 };
