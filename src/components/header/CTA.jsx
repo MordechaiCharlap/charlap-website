1aa84684144c8a200d467b5686c7591be2890711
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-19 17:59:35 +0200

    updated resume download link

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./header.css";
-
+import resume from "../../assets/Moti-Charlap-Resume.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="https://google.com" className="btn">
+      <a href={resume} download className="btn">
         Download CV
       </a>
-      <a href="https://google.com" className="btn btn-primary">
+      <a href="https://wa.me/972508566533" className="btn btn-primary">
         Let's Talk
       </a>
     </div>
