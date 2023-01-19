d7fa8ec0a41c47b2f677eac0f5c7d7aee25fbe17
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-19 17:57:50 +0200

    using my own picture

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CTA from "./CTA";
 import "./header.css";
-import ME from "../../assets/robot-guy.png";
+import ME from "../../assets/me.jpg";
 import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
@@ -9,11 +9,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Moti Charlap</h1>
-        <h5>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-          exercitationem delectus corporis rem eveniet, animi repellat nostrum?
-          Maiores voluptates cupiditate ipsam et excepturi.
-        </h5>
+        <h5>Fullstack developer</h5>
         <CTA />
         <div className="me">
           <img src={ME} alt="Just me" />
