74287fa9e870fdf61bd2899a9d9dd8ee8e4c3cc1
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-24 09:20:25 +0300

    project card responsive af

--- src/components/homePage/portfolio/ProjectCard.jsx
+++ src/components/homePage/portfolio/ProjectCard.jsx
@@ -3,14 +3,16 @@ import "./portfolio.css";
 const ProjectCard = (props) => {
   return (
     <div className="screenshot__card">
-      <h4 className="screenshot__title">{props.title}</h4>
-      <h5 className="screenshot__subtitle">{props.subTitle}</h5>
       <div className="screenshot__image__container">
         <img
           className="screenshot__image"
           alt={"app screenshot"}
           src={props.screenshotImage}
         />
+      </div>
+      <div className="screenshot__text">
+        <h4 className="screenshot__title">{props.title}</h4>
+        <h5 className="screenshot__subtitle">{props.subTitle}</h5>
       </div>
     </div>
   );
