6d03ee575a52cbb4eae3b6aa2a5c4d60070735ea
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-29 15:27:38 +0300

    unneeded file

--- src/components/wedding/reservation.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import reservation from "../../assets/wedding/reservation_.jpg";
-export const Reservation = () => {
-  return (
-    <div className="reservation__container">
-      <img className="reservation" src={reservation} alt="reservation" />
-    </div>
-  );
-};
