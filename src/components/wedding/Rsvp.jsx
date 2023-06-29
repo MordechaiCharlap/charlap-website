06c7106b9d07faa0af3167df6b282721234145d2
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-29 10:00:29 +0300

    using jquery and regex to make much more logical

--- src/components/wedding/Rsvp.jsx
+++ src/components/wedding/Rsvp.jsx
@@ -1,18 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import { HowMany } from "./HowMany";
 import styles from "./wedding.css";
+import $ from "jquery";
 export const Rsvp = () => {
   const [side, setSide] = useState();
   const [phoneNumber, setPhoneNumber] = useState("");
   const [fullName, setFullName] = useState("");
-  const [guestsCount, setGuestsCount] = useState(1);
+  const [guestCount, setGuestCount] = useState(1);
   const emptyInputColor = "#bebebe";
   const filledInputColor = "white";
   const dropdownItemStyle = {
     textAlign: "right",
+  };
+  const fullNameChanged = (text) => {
+    setFullName(text);
+    if (text != "") {
+      $(".fullNameInput").removeClass("errorInput");
+    }
+  };
+  const phoneNumberChanged = (text) => {
+    const regex = /^\d{1,10}$/;
+    if (regex.test(text) || text == "") {
+      setPhoneNumber(text);
+    }
+    if (text != "") {
+      $(".phoneNumberInput").removeClass("errorInput");
+    }
+  };
+  const submitComing = () => {};
+  const submitNotComing = () => {
+    var isValid = true;
+    if (fullName === "") {
+      $(".fullNameInput").addClass("errorInput");
+      isValid = false;
+    }
+    const validPhoneNumberRegex = /^\d{10}$/;
+    if (phoneNumber === "" || !validPhoneNumberRegex.test(phoneNumber)) {
+      $(".phoneNumberInput").addClass("errorInput");
+      isValid = false;
+    }
+    if (!side) {
+      $("#dropdown-basic-button").addClass("errorInput");
+    }
+    if (!isValid) return;
+  };
+  const sideChanged = (sideChange) => {
+    setSide(sideChange);
+    if (sideChange) {
+      console.log(sideChange);
+      $("#dropdown-basic-button").removeClass("errorInput");
+    }
   };
   return (
     <div dir="rtl" id="rsvp">
@@ -20,39 +60,28 @@ export const Rsvp = () => {
         <DropdownButton id="dropdown-basic-button" title={side || "בחרו צד"}>
           <Dropdown.Item
             style={dropdownItemStyle}
-            onClick={() => setSide("משפחה תם")}
+            onClick={() => sideChanged("משפחה תם")}
           >
-            משפחה תם
+            תם - משפחה
           </Dropdown.Item>
           <Dropdown.Item
             style={dropdownItemStyle}
-            onClick={() => setSide("משפחה מוטי")}
+            onClick={() => sideChanged("החברים של תם")}
           >
-            משפחה מוטי
+            תם - חברים
           </Dropdown.Item>
           <Dropdown.Item
             style={dropdownItemStyle}
-            onClick={() => setSide("החברים של תם")}
+            onClick={() => sideChanged("משפחה מוטי")}
           >
-            החברים של תם
+            מוטי - משפחה
           </Dropdown.Item>
+
           <Dropdown.Item
             style={dropdownItemStyle}
-            onClick={() => setSide("ההורים של תם")}
+            onClick={() => sideChanged("ההורים של תם")}
           >
-            ההורים של תם
-          </Dropdown.Item>
-          <Dropdown.Item
-            style={dropdownItemStyle}
-            onClick={() => setSide("החברים של מוטי")}
-          >
-            החברים של מוטי
-          </Dropdown.Item>
-          <Dropdown.Item
-            style={dropdownItemStyle}
-            onClick={() => setSide("ההורים של מוטי")}
-          >
-            ההורים של מוטי
+            מוטי - חברים
           </Dropdown.Item>
         </DropdownButton>
         <div
@@ -61,8 +90,9 @@ export const Rsvp = () => {
           }}
         />
         <input
+          className="fullNameInput"
           onChange={(e) => {
-            setFullName(e.nativeEvent.target.value);
+            fullNameChanged(e.nativeEvent.target.value);
           }}
           style={{
             borderRadius: 8,
@@ -74,8 +104,10 @@ export const Rsvp = () => {
         />
         <div style={{ height: 10 }} />
         <input
+          value={phoneNumber}
+          className="phoneNumberInput"
           onChange={(e) => {
-            setPhoneNumber(e.nativeEvent.target.value);
+            phoneNumberChanged(e.nativeEvent.target.value);
           }}
           style={{
             borderRadius: 8,
@@ -86,14 +118,25 @@ export const Rsvp = () => {
           placeholder="מספר טלפון"
         />
         <div style={{ height: 10 }} />
-        <HowMany />
+        <HowMany setGuestCount={setGuestCount} />
         <div style={{ height: 10 }} />
         <div className="coming__or__not">
-          <Button variant="success">מגיעים</Button>
+          <Button className="coming__button" variant="success">
+            מגיעים
+          </Button>
           <div style={{ width: 10 }} />
-          <Button variant="danger">לא מגיעים</Button>
+          <Button
+            onClick={submitNotComing}
+            className="not__coming__button"
+            variant="danger"
+          >
+            לא מגיעים
+          </Button>
         </div>
       </div>
+      <p style={{ width: 400 }}>
+        *אם אתם רוצים לעדכן את האישור שלכם מלאו את הטופס שוב עם אותו המספר טלפון
+      </p>
     </div>
   );
 };
