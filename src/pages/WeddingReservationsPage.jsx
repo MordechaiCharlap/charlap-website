957310183bc85a3022556ffb6c75b3885eea45eb
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-29 23:08:03 +0300

    id bug fixed

--- src/pages/WeddingReservationsPage.jsx
+++ src/pages/WeddingReservationsPage.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Rsvp } from "../components/wedding/Rsvp";
 import reservation from "../assets/wedding/reservation_.jpg";
+import us from "../assets/wedding/tom-and-me.jpg";
 import { Button } from "react-bootstrap";
 import "../components/wedding/wedding.css";
 const WeddingReservationsPage = () => {
   return (
-    <div id="#weddingReservationStart" className="weddingReservationsPage">
+    <div
+      id="wedding-reservation-start"
+      className="weddingReservationsPage"
+      style={{ backgroundColor: "gray" }}
+    >
       <div className="container" style={{}}>
         <div className="fullPage align-items-center">
-          <img className="reservation" src={reservation} alt="reservation" />
-          <div style={{ height: 10 }} />
+          <img className="reservation" src={us} alt="reservation" />
+          <div className="heightMargin" />
           <Button variant="secondary" size="lg" href="#rsvp">
             אישור/ביטול הגעה
           </Button>
