0f83daf499c5b0f2183ef8bda80cd7b1cb03371a
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-29 15:28:12 +0300

    DELETED SO MUCH CODE

--- src/pages/WeddingReservationsPage.jsx
+++ src/pages/WeddingReservationsPage.jsx
@@ -1,35 +1,18 @@
 import React from "react";
 import { Rsvp } from "../components/wedding/Rsvp";
-import US from "../assets/wedding/tom-and-me.jpg";
 import reservation from "../assets/wedding/reservation_.jpg";
 import { Button } from "react-bootstrap";
 import "../components/wedding/wedding.css";
 const WeddingReservationsPage = () => {
   return (
-    <div
-      className="weddingReservationsPage"
-      style={{ flex: 1, backgroundColor: "grey" }}
-    >
-      <div
-        className="container"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        {/* <div className="wedding__container">
-              <img className="our__image" src={US} alt="me and tom" />
-              
-        <h1 style={{ zIndex: 999 }}>החתונה של תם ומוטי</h1>
-      </div> */}
-        <div className="reservation__and__rsvp__button">
+    <div id="#weddingReservationStart" className="weddingReservationsPage">
+      <div className="container" style={{}}>
+        <div className="fullPage align-items-center">
           <img className="reservation" src={reservation} alt="reservation" />
-          <div>
-            <Button variant="secondary" size="lg" href="#rsvp">
-              אישור/ביטול הגעה
-            </Button>
-          </div>
+          <div style={{ height: 10 }} />
+          <Button variant="secondary" size="lg" href="#rsvp">
+            אישור/ביטול הגעה
+          </Button>
         </div>
         <Rsvp />
       </div>
