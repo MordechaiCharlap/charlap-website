144533c1a60af5be705f8f5537d587ed1479c1ca
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-04 23:11:38 +0300

    much more comftable to play with+AddToList

--- src/components/wedding/control-panel/ListTable.jsx
+++ src/components/wedding/control-panel/ListTable.jsx
@@ -1,14 +1,29 @@
 import { doc, updateDoc } from "firebase/firestore";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
+import { AddToList } from "./AddToList";
+import { gridColumns } from "../defaultValues";
 
 export const ListTable = ({ db, currentList, weddingData }) => {
-  const gridColumns = [
-    ["fullName", "שם מלא"],
-    ["guestCount", "כמה אתם"],
-    ["isComing", "מגיעים?"],
-    ["side", "צד"],
-  ];
+  const [comingCount, setComingCount] = useState(0);
+  const [notComingCount, setNotComingCount] = useState(0);
+  const [needUpdate, setNeedUpdate] = useState(true);
+  useEffect(() => {
+    if (needUpdate) return;
+    console.log("updating counters");
+    var tempComing = 0;
+    var tempNotComing = 0;
+    weddingData[currentList].forEach((rsvp) => {
+      if (rsvp.isComing) {
+        tempComing += rsvp.guestCount;
+      } else {
+        tempNotComing += rsvp.guestCount;
+      }
+    });
+    setComingCount(tempComing);
+    setNotComingCount(tempNotComing);
+    setNeedUpdate(false);
+  }, [needUpdate]);
   const deleteFromList = async (index) => {
     const listClone = weddingData[currentList];
     listClone.splice(index, 1);
@@ -18,33 +33,48 @@ export const ListTable = ({ db, currentList, weddingData }) => {
   };
   return (
     <div dir="rtl">
-      <h3>
-        כמות אנשים כללית:{" "}
-        {weddingData[currentList].reduce((accumulator, currentValue) => {
-          return accumulator + currentValue.guestCount;
-        }, 0)}
-      </h3>
+      {currentList == "guestList" ? (
+        <div>
+          <div className="d-flex">
+            <h3>מגיעים: {comingCount}</h3>
+            <h3>לא מגיעים: {notComingCount}</h3>
+          </div>
+          <AddToList
+            db={db}
+            sides={weddingData.sides}
+            setNeedUpdate={setNeedUpdate}
+          />
+        </div>
+      ) : (
+        <h3 className="text-center">
+          כמות אנשים כללית:{" "}
+          {weddingData[currentList].reduce((accumulator, currentValue) => {
+            return accumulator + currentValue.guestCount;
+          }, 0)}
+        </h3>
+      )}
+
       <table dir="rtl" className="table table-striped">
         <thead>
           <tr>
             {gridColumns.map((colName, index) => {
               return (
-                <th key={index} className="text-center col-3 border">
+                <th key={index} className="text-center border">
                   {colName[1]}
                 </th>
               );
             })}
 
-            <th className="text-center col-3 border">פעולה</th>
+            <th className="text-center border">פעולה</th>
           </tr>
         </thead>
         <tbody>
           {weddingData[currentList].map((rsvp, index) => {
             return (
               <tr key={`${currentList}/${index}`}>
-                {gridColumns.map((colName) => {
+                {gridColumns.map((colName, index) => {
                   return (
-                    <th className="text-center align-middle">
+                    <th key={index} className="text-center align-middle">
                       {typeof rsvp[colName[0]] == "boolean"
                         ? rsvp[colName[0]] == true
                           ? "כן"
@@ -53,7 +83,7 @@ export const ListTable = ({ db, currentList, weddingData }) => {
                     </th>
                   );
                 })}
-                <th>
+                <th className="text-center">
                   <Button
                     className="btn-danger"
                     onClick={async () => {
