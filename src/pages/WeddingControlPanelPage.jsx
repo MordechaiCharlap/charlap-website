52fafa82868346b93b51229848481743741dc327
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-04 23:12:22 +0300

    grid colums in external file

--- src/pages/WeddingControlPanelPage.jsx
+++ src/pages/WeddingControlPanelPage.jsx
@@ -2,21 +2,15 @@ import React, { useEffect } from "react";
 import "../components/wedding/control-panel/wedding-control-panel.css";
 import { firestore } from "../firebase.config";
 import { useState } from "react";
-import { doc, onSnapshot, updateDoc } from "firebase/firestore";
+import { doc, onSnapshot } from "firebase/firestore";
 
 import { ListMenu } from "../components/wedding/control-panel/ListMenu";
-import { Button } from "react-bootstrap";
 import { ListTable } from "../components/wedding/control-panel/ListTable";
 export const WeddingControlPanelPage = () => {
   const db = firestore;
   const [weddingData, setWeddingData] = useState();
   const [currentList, setCurrentList] = useState("guestList");
-  const gridColumns = [
-    ["fullName", "שם מלא"],
-    ["guestCount", "כמה אתם"],
-    ["isComing", "מגיעים?"],
-    ["side", "צד"],
-  ];
+
   useEffect(() => {
     const unsub = onSnapshot(doc(db, "wedding/allData"), (doc) => {
       const data = doc.data();
