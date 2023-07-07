7b87f76159c6f3cdfc0c21d52a87f738f7b66a53
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-07 20:16:32 +0300

    cleaner better code

--- src/components/wedding/control-panel/AddToList.jsx
+++ src/components/wedding/control-panel/AddToList.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import { gridColumns } from "../defaultValues";
-import { Button, DropdownButton } from "react-bootstrap";
-import { Dropdown } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import $ from "jquery";
 import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { IsComingDropdown } from "./IsComingDropdown";
+import { SideDropdown } from "./SideDropdown";
 export const AddToList = ({ sides, db }) => {
   const [fullName, setFullName] = useState("");
   const [guestCount, setGuestCount] = useState(0);
-  const [isComing, setIsComing] = useState(false);
+  const [isComing, setIsComing] = useState("maybe");
   const [side, setSide] = useState();
+  const [phoneNumber, setPhoneNumber] = useState("");
   const [errorText, setErrorText] = useState(" ");
   const dropdownItemStyle = {
     width: 200,
@@ -22,10 +24,10 @@ export const AddToList = ({ sides, db }) => {
   };
   const clearInputs = () => {
     setFullName("");
-    setIsComing(false);
+    setIsComing("maybe");
     setGuestCount(0);
-    setIsComing(false);
     setSide();
+    setPhoneNumber("");
   };
   const checkIsValid = () => {
     if (fullName == "" || guestCount == 0 || side == null) {
@@ -41,25 +43,30 @@ export const AddToList = ({ sides, db }) => {
     const newValue = {
       fullName: fullName,
       guestCount: guestCount,
-      isComing: isComing == true ? "yes" : isComing == false ? "no" : "maybe",
+      isComing: isComing,
       side: side,
+      phoneNumber: phoneNumber,
     };
 
     clearInputs();
-    console.log(newValue);
     await updateDoc(doc(db, "wedding/allData"), {
       guestList: arrayUnion(newValue),
     });
   };
+  const thStyle = {
+    textAlign: "center",
+    borderColor: "#0b0c10",
+    borderWidth: 1,
+  };
   return (
-    <div className="bg-secondary rounded m-5 p-5">
-      <h3 className="text-center">הכנסת אורחים ידנית</h3>
+    <div className="rounded m-5 p-5" style={{ backgroundColor: "#cbcbcb" }}>
+      <h3 className="text-center mb-3">הכנסת אורחים ידנית</h3>
       <table className="table">
         <thead>
           <tr>
             {gridColumns.map((colName, index) => {
               return (
-                <th key={index} className="text-center border col-3">
+                <th key={index} style={thStyle} className="col-3">
                   {colName[1]}
                 </th>
               );
@@ -68,14 +75,21 @@ export const AddToList = ({ sides, db }) => {
         </thead>
         <tbody>
           <tr className="align-middle">
-            <th className="border text-center">
+            <th style={thStyle}>
               <input
                 value={fullName}
                 onChange={(e) => setFullName(e.target.value)}
                 className="rounded text-center"
               />
             </th>
-            <th className="border text-center">
+            <th style={thStyle}>
+              <input
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                className="rounded text-center"
+              />
+            </th>
+            <th style={thStyle}>
               <input
                 value={guestCount}
                 onChange={(e) =>
@@ -87,66 +101,11 @@ export const AddToList = ({ sides, db }) => {
                 className="rounded text-center"
               />
             </th>
-            <th className="border text-center">
-              <DropdownButton
-                style={{
-                  width: "100%",
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-                className="dropdown-not-filled"
-                id="dropdown-basic-button"
-                title={
-                  isComing == true
-                    ? "כן"
-                    : isComing == false
-                    ? "לא"
-                    : "לא ידוע עדיין"
-                }
-              >
-                <Dropdown.Item
-                  style={dropdownItemStyle}
-                  onClick={() => setIsComing(true)}
-                >
-                  כן
-                </Dropdown.Item>
-                <Dropdown.Item
-                  style={dropdownItemStyle}
-                  onClick={() => setIsComing(false)}
-                >
-                  לא
-                </Dropdown.Item>
-                <Dropdown.Item
-                  style={dropdownItemStyle}
-                  onClick={() => setIsComing()}
-                >
-                  לא ידוע עדיין
-                </Dropdown.Item>
-              </DropdownButton>
+            <th style={thStyle}>
+              <IsComingDropdown setIsComing={setIsComing} />
             </th>
-            <th className="border text-center">
-              <DropdownButton
-                style={{
-                  width: "100%",
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-                className="dropdown-not-filled"
-                id="dropdown-basic-button"
-                title={side || "בחרו צד"}
-              >
-                {sides.map((side, index) => {
-                  return (
-                    <Dropdown.Item
-                      key={side}
-                      style={dropdownItemStyle}
-                      onClick={() => sideChanged(side)}
-                    >
-                      {side}
-                    </Dropdown.Item>
-                  );
-                })}
-              </DropdownButton>
+            <th style={thStyle}>
+              <SideDropdown sides={sides} setSide={sideChanged} />
             </th>
           </tr>
         </tbody>
